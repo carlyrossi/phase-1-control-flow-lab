062bcecb8a7349e69d7e409b046d0560afce79d9
Carly <+>
2022-01-12 21:54:05 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(anyValue){
+    if (anyValue <= 400)
+    return ('This one is on me!')
+
+if (anyValue > 2000 && anyValue <= 2500)
+return ('I will gladly take your thirty bucks.')
+ 
+if (anyValue > 2500)
+return ('No can do.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(anyCity){
+ if (anyCity == 'NYC')
+ return ('Ok, sounds good.')
+
+ if (anyCity !== 'NYC')
+ return ('No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(cashMoney){
+  if (cashMoney == 'generous')
+ return ('Thank you so much.')
+
+ if (cashMoney == 'not as generous')
+ return ('Thank you.')
+
+ if (cashMoney !== 'generous' && 'not as generous')
+ return ('Bye.')
 }
